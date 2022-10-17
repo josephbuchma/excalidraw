8f59ea244a93f04a31cd3799dd025b5a6fe773e7
Yosyp Buchma <yo@yosyp.co>
2022-10-17 12:43:13 +0300

    fix: proper body height in browser & PWA mode

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,15 @@ import "./excalidraw-app/sentry";
 window.__EXCALIDRAW_SHA__ = process.env.REACT_APP_GIT_SHA;
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
+
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", appHeight);
+
+appHeight();
+
 root.render(
   <StrictMode>
     <ExcalidrawApp />
